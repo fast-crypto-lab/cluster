b8c6a43782ec0f7fd03328edc53ad8f06ccff314
Bo-Yuan Peng <boyuanpeng@gmail.com>
2018-12-20 16:12:59 +0800

    XDDDDD

--- _update_settings_on_this_machine_suseT.py
+++ _update_settings_on_this_machine_suseT.py
@@ -50,8 +50,8 @@ def assert_good_environment():
     assert sys.version_info.major == 3
     assert sys.version_info.minor >= 6
     # Ensure you are running this on a Ubuntu 18.04 server
-    print 'Platform: {0}'.format(platform.dist()[0])
-    print 'Version: {0}'.format(platform.dist()[1])
+    print('Platform: {0}'.format(platform.dist()[0]))
+    print('Version: {0}'.format(platform.dist()[1]))
     #assert platform.dist()[0] == 'Ubuntu'
     #assert platform.dist()[1].startswith('18.04')
 
